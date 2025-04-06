6bea62ff6a2fa850503aad60c1455d0aa133de34
Abdullah <abdullah.atkaev@gmail.com>
2025-04-06 13:14:29 +0300

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
                 href="/about"
                 className="text-sm lowercase hover:opacity-70 transition-opacity"
               >
-                о boomzi
+                о Atkaev REHAB
               </Link>
             </li>
             <li>
@@ -32,15 +32,6 @@ const Footer = () => {
             </li>
           </ul>
 
-          <div className="mt-6 mb-4">
-            <img
-              src="https://ext.same-assets.com/1334220929/2722572661.png"
-              alt="Boomzi Logo"
-              width={150}
-              height={15}
-              className="h-4 w-auto"
-            />
-          </div>
         </div>
 
         <div className="flex justify-center border-t border-gray-100 pt-4">
