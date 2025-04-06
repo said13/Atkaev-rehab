1f1f380f55c01be6911f1bcf0201305cfc8fd9d7
Abdullah <abdullah.atkaev@gmail.com>
2025-04-06 12:47:46 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,6 @@ export default function Home() {
     <PageLayout>
       <div className="boomzi-bg py-16">
         <div className="boomzi-container">
-          <div className="hidden md:flex justify-center mb-12 space-x-6 text-sm">
-            <button className="hover:opacity-75 transition-opacity">Все</button>
-            <button className="hover:opacity-75 transition-opacity">
-              Футболки
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Свитеры
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Лонгсливы
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Худи
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Постеры
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Брелки
-            </button>
-            <button className="hover:opacity-75 transition-opacity">
-              Ковры
-            </button>
-          </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {products.map((product) => (
